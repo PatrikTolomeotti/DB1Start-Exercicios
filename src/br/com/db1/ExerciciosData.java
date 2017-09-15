b6a396a8d9ef82b57cff48ac7be176a6082853e0
Patrik Tolomeotti <patrik.tolomeotti@live.com>
2017-09-15 13:51:41 -0300

    Melhora nos códigos

--- src/br/com/db1/ExerciciosData.java
+++ src/br/com/db1/ExerciciosData.java
@@ -10,9 +10,7 @@ import java.util.Date;
 public class ExerciciosData {
 
 	public Boolean identificarAnoBisexto(Integer ano) {
-		if(ano%400 == 0){
-			return true;
-		} else if(ano%4 == 0 && ano%100 !=0){
+		if(ano%400 == 0 | ano%4 == 0 && ano%100 !=0){
 			return true;
 		}
 		else {
